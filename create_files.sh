2d0b63a373524cca4144f3362bd9c665260c85ce
Peter Reuterås <peter@reuteras.com>
2016-03-12 20:52:36 +0100

    Write smaller files.

--- create_files.sh
+++ create_files.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-TEXT="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit amet elit pharetra tincidunt feugiat nisl imperdiet. Ut convallis libero in urna ultrices accumsan. Donec sed odio eros. Donec viverra mi quis quam pulvinar at malesuada arcu rhoncus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In rutrum accumsan ultricies. Mauris vitae nisi at sem facilisis semper ac in est."
+TEXT="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem."
 
 for ((j=0; j < 5000; j++)); do 
-    echo "This is the secret in file $j in root. \n$TEXT" > secret_file_"$j".txt;
+    echo "Secret file $j in root. \n$TEXT" > secret_file_"$j".txt;
 done
 
 i=0
 while true; do
     mkdir dir"$i"
     for ((j=0; j < 5000; j++)); do 
-        echo "This is the secret in file $j in dir $i. \n$TEXT" > dir"$i"/secret_file_"$j".txt;
+        echo "Secret file $j in dir $i. \n$TEXT" > dir"$i"/secret_file_"$j".txt;
     done
     i=$(( i + 1 ))
 done
