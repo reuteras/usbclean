2bf641c1342d6a3f13a1836d35cc56aedeadda94
Peter Reuterås <peter@reuteras.com>
2024-05-09 08:26:57 +0200

    Fix linter issues

--- create_files.sh
+++ create_files.sh
@@ -4,7 +4,7 @@ set -e
 
 TEXT="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut gravida lorem."
 
-for ((j=0; j < 5000; j++)); do
+for ((j = 0; j < 5000; j++)); do
     # shellcheck disable=SC1117
     # shellcheck disable=SC2028
     echo "Secret file $j in root. \n$TEXT" > secret_file_"$j".txt
